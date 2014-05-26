67d93306b132ba5ec579648daf6e0defa77c14ad
xinyuexrow <wang@xrow.de>
2014-05-26 11:39:28 +0200

    Code optimization

--- Command/CreateTweetContentCommand.php
+++ Command/CreateTweetContentCommand.php
@@ -29,7 +29,7 @@ class CreateTweetContentCommand extends ContainerAwareCommand
         $twitterAccounts = $input->getArgument( 'twitterAccount' );
         $repository = $this->getContainer()->get( 'ezpublish.api.repository' );
         $repository->setCurrentUser(
-            $repository->getUserService()->loadUserByLogin( 'admin' )
+            $repository->getUserService()->loadUserByLogin( 'xxxxx' )
         );
 
         $contentService = $repository->getContentService();
@@ -37,33 +37,40 @@ class CreateTweetContentCommand extends ContainerAwareCommand
         //Getting a twitter user's latest posts using Guzzle
         $twitterClient = $this->getApplication()->getKernel()->getContainer()->get('guzzle.twitter.client');
         $status = $twitterClient->get('statuses/user_timeline.json');
-        
+        $tweets_array=array();
         foreach($twitterAccounts as $twitterAccount)
         {
             $status->getQuery()->set('screen_name', $twitterAccount);
             $response = $status->send();
+            
+            $tweets = json_decode($response->getBody(),true);
+            foreach($tweets as $tweets_one)
+            {
+                $index= strtotime($tweets_one['created_at']);
+                $tweets_array[$index] = $tweets_one ;
+                ksort($tweets_array,SORT_NUMERIC);
+            }
+        }
         
-            $tweets = json_decode($response->getBody());
+        foreach($tweets_array as $tweet)
+        {
             $currentTime = time();
-            foreach($tweets as $tweet){
-              $tweetTimestamp = strtotime($tweet->created_at);
-              if($currentTime-$tweetTimestamp<86400){
-
-                $tweetId="twitter::tweet::" . $tweet->id_str;
-                $tweet_pretty=$this->linkEntitiesWithinText($tweet);
-                
+            $tweetTimestamp = strtotime($tweet['created_at']);
+            if($currentTime-$tweetTimestamp<3600)
+            {
+                $tweetId="twitter::tweet::" . $tweet['id_str'];
+                $tweet_pretty = $this->parse_message( $tweet );
                // Content create struct
                 $createStruct = $contentService->newContentCreateStruct(
                     $repository->getContentTypeService()->loadContentTypeByIdentifier( 'tweets' ),
                     'ger-DE'
                 );
 
-               // $createStruct->setField( 'text',$tweet->text , 'ger-DE' );
                 $createStruct->setField( 'text',$tweet_pretty, 'ger-DE' );
                 $createStruct->setField( 'tweet', json_encode($tweet), 'ger-DE' );
 
                 $createStruct->remoteId = $tweetId;
-                    
+                
                 try
                 {
                     $contentDraft = $contentService->createContent(
@@ -71,6 +78,7 @@ class CreateTweetContentCommand extends ContainerAwareCommand
                      );
                     $content = $contentService->publishVersion( $contentDraft->versionInfo );
                     $output->writeln( "Created Content 'tweet' with (Object)ID {$content->id}" );
+                    sleep(2);
                 }
                 catch ( \Exception $e )
                 {
@@ -78,43 +86,64 @@ class CreateTweetContentCommand extends ContainerAwareCommand
                     //$output->writeln( $e->getTraceAsString() );
                     continue;
                 }
-            }else{$output->writeln( "No new tweets being displayed...." );}
-          }
-        }
+             }else{$output->writeln( "No new tweets being displayed...." );}
+         }
     }
     
-    function linkEntitiesWithinText($apiResponseTweetObject) 
+    function parse_message( &$tweet ) 
     {
-        $characters = str_split($apiResponseTweetObject->text);
-    
-        //for @user_mentions
-        foreach ($apiResponseTweetObject->entities->user_mentions as $entity) {
-            $link = "https://twitter.com/" . $entity->screen_name;
-            $characters[$entity->indices[0]] = "<a href=\"$link\">" . $characters[$entity->indices[0]];
-            $characters[$entity->indices[1] - 1] .= "</a>";
+        if ( !empty($tweet['entities']) ) 
+        {
+            $replace_index = array();
+            $append = array();
+            $text = $tweet['text'];
+            foreach ($tweet['entities'] as $area => $items) 
+            {
+                $prefix = false;
+                $display = false;
+                switch ( $area ) {
+                    case 'hashtags':
+                        $find = 'text';
+                        $prefix = '#';
+                        $url = 'https://twitter.com/search/?src=hash&q=%23';
+                        break;
+                    case 'user_mentions':
+                        $find = 'screen_name';
+                        $prefix = '@';
+                        $url = 'https://twitter.com/';
+                        break;
+                    /*case 'media':
+                        $display = 'media_url_https';
+                        $href = 'media_url_https';
+                        $size = 'small';
+                        break;*/
+                    case 'urls':
+                        $find = 'url';
+                        $display = 'display_url';
+                        $url = "expanded_url";
+                        break;
+                    default: break;
+                }
+                foreach ($items as $item) 
+                {
+                  /*if ( $area == 'media' ) 
+                   *{
+                        // $append[$item->$display] = "<img src=\"{$item->$href}:$size\" />";
+                    }else{*/
+                    $msg = $display ? $prefix.$item[$display] : $prefix.$item[$find];
+                    $replace = $prefix.$item[$find];
+                    $href = isset($item[$url]) ? $item[$url] : $url;
+                    if (!(strpos($href, 'http') === 0)) $href = "http://".$href;
+                    if ( $prefix ) $href .= $item[$find];
+                    $with = "<a href=\"$href\">$msg</a>";
+                    $replace_index[$replace] = $with;
+                 //}
+                }
+            }
+            foreach ($replace_index as $replace => $with) $tweet['text'] = str_replace($replace,$with,$tweet['text']);
+            foreach ($append as $add) $tweet['text'] .= $add;
+        
+            return $tweet['text'];
         }
-    
-        //for #hashtags
-        foreach ($apiResponseTweetObject->entities->hashtags as $entity) {
-            $link = "https://twitter.com/search?q=%23" . $entity->text;
-            $characters[$entity->indices[0]] = "<a href=\"$link\">" . $characters[$entity->indices[0]];
-            $characters[$entity->indices[1] - 1] .= "</a>";
-        }
-    
-        //for urls
-        foreach ($apiResponseTweetObject->entities->urls as $entity) {
-            $link = $entity->expanded_url;
-            $characters[$entity->indices[0]] = "<a href=\"$link\">" . $characters[$entity->indices[0]];
-            $characters[$entity->indices[1] - 1] .= "</a>";
-        }
-    
-        //for media
-        /*foreach ($apiResponseTweetObject->entities->media as $entity) {
-            $link = $entity->expanded_url;
-            $characters[$entity->indices[0]] = "<a href=\"$link\">" . $characters[$entity->indices[0]];
-            $characters[$entity->indices[1] - 1] .= "</a>";
-        }*/
-
-        return implode('', $characters);
     }
 }
